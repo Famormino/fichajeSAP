1167aa79597bcc6eaef18375e3d9427c76b9808a
Federico Amormino <federico.amormino@gmail.com>
2023-11-09 10:48:26 -0300

    adding copy to clipboard

--- src/components/Planilla.jsx
+++ src/components/Planilla.jsx
@@ -7,11 +7,38 @@ import "./Planilla.css";
 
 const Planilla = ({ fechaInicio, fechaFin }) => {
     const [diasPlanilla, setDíasPlanilla] = useState([]);
-    console.log(diasPlanilla);
     const dias = obtenerFechasSinFeriadosYSabadosDomingos(
         fechaInicio,
         fechaFin
     );
+
+    const copyTable = () => {
+        const elTable = document.querySelector("table");
+
+        let range, sel;
+
+        // Ensure that range and selection are supported by the browsers
+        if (document.createRange && window.getSelection) {
+            range = document.createRange();
+            sel = window.getSelection();
+            // unselect any element in the page
+            sel.removeAllRanges();
+
+            try {
+                range.selectNodeContents(elTable);
+                sel.addRange(range);
+            } catch (e) {
+                range.selectNode(elTable);
+                sel.addRange(range);
+            }
+
+            document.execCommand("copy");
+        }
+
+        sel.removeAllRanges();
+
+        console.log("Element Copied! Paste it in a file");
+    };
 
     useEffect(() => {
         setDíasPlanilla(
@@ -70,6 +97,7 @@ const Planilla = ({ fechaInicio, fechaFin }) => {
                 </tbody>
             </table>
             <button onClick={exportFile}>Exportar a Excel</button>
+            <button onClick={copyTable}>Copiar tabla</button>
         </div>
     );
 };
