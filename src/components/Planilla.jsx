0fb78c6876afb400cffa64298f46093665d59998
Federico Amormino <federico.amormino@gmail.com>
2023-11-10 08:21:46 -0300

    adding favicon

--- src/components/Planilla.jsx
+++ src/components/Planilla.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { utils, writeFile } from "xlsx";
 
 import { obtenerFechasSinFeriadosYSabadosDomingos } from "../helpers/diasHabiles";
